98aab3f4a2571d3163b8566a1c2c93e3fac3055a
aliijaz1997 <57563582+aliijaz1997@users.noreply.github.com>
2020-09-09 18:39:40 +0500

    firebase deployed

--- public/firebase-messaging-sw.ts
+++ public/firebase-messaging-sw.ts
@@ -2,14 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/7.17.1/firebase-messaging.js');
 
 firebase.initializeApp( {
-    apiKey: "AIzaSyATgmxFbbak-TW-bv5KVEJXjtFWI-E9mYI",
-    authDomain: "timer-app-usingtdd.firebaseapp.com",
-    databaseURL: "https://timer-app-usingtdd.firebaseio.com",
-    projectId: "timer-app-usingtdd",
-    storageBucket: "timer-app-usingtdd.appspot.com",
-    messagingSenderId: "408675936992",
-    appId: "1:408675936992:web:71d16bbc9ef4c8756d3e85"
-  });
+  apiKey: "AIzaSyATgmxFbbak-TW-bv5KVEJXjtFWI-E9mYI",
+  authDomain: "timer-app-usingtdd.firebaseapp.com",
+  databaseURL: "https://timer-app-usingtdd.firebaseio.com",
+  projectId: "timer-app-usingtdd",
+  storageBucket: "timer-app-usingtdd.appspot.com",
+  messagingSenderId: "408675936992",
+  appId: "1:408675936992:web:3684e7eb98468f256d3e85"
+});
   // Calling service worker here
 
   firebase.messaging();
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-
-
-describe("App", () => {
-  let component: any;
-  beforeEach(() => component = shallow(<App />));
-  it("It should render ", () => {
-    expect(component.find("div").length).toBeGreaterThanOrEqual(1);
-  })
-
-  it ("It should have buttons", () => {
-    expect(component.find("button").length).toEqual(3)
-  })
-  // it ("It should have buttons", () => {
-  //   expect(component.find("start").length).toBeGreaterThanOrEqual(1);
-  // })
-})
+test('renders the component', () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
+});
